a0a9d65e44377b2b453bb1a7fe741bd953a74684
Sam Tregar <sam@tregar.com>
2020-06-21 13:43:49 -0700

    Increment version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="holidays-plus", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Sam Tregar",
     author_email="sam@tregar.com",
     description="Enhanced add-on for the holidays library offering expanded functionality",
