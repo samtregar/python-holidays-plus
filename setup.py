2394f8795e97ca01fbef481ceb0ec4805b9c0eaf
Sam Tregar <sam@tregar.com>
2020-06-21 13:44:21 -0700

    Remove some boilerplate.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="holidays-plus", # Replace with your own username
+    name="holidays-plus",
     version="0.0.2",
     author="Sam Tregar",
     author_email="sam@tregar.com",
